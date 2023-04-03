95a89502a01c8c125d648438d75bea809dff6f14
Sam Germain <sam.germain@usask.ca>
2023-04-03 19:05:44 -0400

    feat(throttle): rate limiter can be set to always max out tokens available

--- ts/src/base/functions/throttle.ts
+++ ts/src/base/functions/throttle.ts
@@ -14,22 +14,28 @@ class Throttler {
             'maxCapacity': 2000,
             'tokens': 0,
             'cost': 1.0,
+            'throttleLimit': false, // set to true to use the maximum available rate limit from the beginning
         };
         Object.assign (this.config, config);
-        this.queue = [];
+        this.queue = []; // requests to be sent
         this.running = false;
     }
 
     async loop () {
         let lastTimestamp = now ();
-        while (this.running) {
+        while (this.running) { // loops through method calls in the queue, executing them if tokens available, and waiting if tokens not available
             const { resolver, cost } = this.queue[0];
-            if (this.config['tokens'] >= 0) {
+            if (this.config['tokens'] >= 0) { // if rate limit hasn't been reached
                 this.config['tokens'] -= cost;
-                resolver ();
+                this.resolve (resolver);
+                resolver ();  // method for api endpoint
                 this.queue.shift ();
                 // contextswitch
-                await Promise.resolve ();
+                if (this.config[throttleLimit]) {
+                    Promise.resolve ();
+                } else {
+                    await Promise.resolve ();
+                }
                 if (this.queue.length === 0) {
                     this.running = false;
                 }
