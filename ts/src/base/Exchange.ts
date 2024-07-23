b0097a69a48888b842bce66b17a0ea10f1435252
Sam Germain <sam.germain@usask.ca>
2024-07-22 20:16:23 -0400

    chore: parseBalance types

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -4564,7 +4564,7 @@ export default class Exchange {
         throw new NotSupported (this.id + ' fetchBalanceWs() is not supported yet');
     }
 
-    parseBalance (response): Balances {
+    parseBalance (response: Dict): Balances {
         throw new NotSupported (this.id + ' parseBalance() is not supported yet');
     }
 
