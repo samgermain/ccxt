20caf0cafe0f0866648ba07cb4502eac180eb95b
Sam Germain <sam.germain@usask.ca>
2025-01-17 04:48:56 -0600

    feat(idex): fetchOHLCV - params["until"]

--- ts/src/idex.ts
+++ ts/src/idex.ts
@@ -463,6 +463,7 @@ export default class idex extends Exchange {
      * @param {int} [since] timestamp in ms of the earliest candle to fetch
      * @param {int} [limit] the maximum amount of candles to fetch
      * @param {object} [params] extra parameters specific to the exchange API endpoint
+     * @param {int} [params.until] timestamp in ms of the latest candle to fetch
      * @returns {int[][]} A list of candles ordered as timestamp, open, high, low, close, volume
      */
     async fetchOHLCV (symbol: string, timeframe = '1m', since: Int = undefined, limit: Int = undefined, params = {}): Promise<OHLCV[]> {
@@ -472,12 +473,17 @@ export default class idex extends Exchange {
             'market': market['id'],
             'interval': timeframe,
         };
+        const until = this.safeInteger (params, 'until');
         if (since !== undefined) {
             request['start'] = since;
         }
         if (limit !== undefined) {
             request['limit'] = Math.min (limit, 1000);
         }
+        if (until !== undefined) {
+            request['end'] = until;
+        }
+        params = this.omit (params, 'until');
         const response = await this.publicGetCandles (this.extend (request, params));
         if (Array.isArray (response)) {
             // [
